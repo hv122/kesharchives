e68be7a4b6fb495f3d332862c14a287c94464036
Hrishikesh Venkatesh <115477237+hv122@users.noreply.github.com>
2023-07-20 23:24:10 +0000

    added comments

--- heapoverstack.cpp
+++ heapoverstack.cpp
@@ -5,7 +5,8 @@ int *getData()
     int *a = new int(5);
     return a;
 } // we use new, which adds to the heap instead of the stack
-  // as we used new, we need to delete the memory manually.
+  // as we used new, we need to delete the memory manually,
+  // smart pointers allow us to avoid manual mem management
 
 int main()
 {
